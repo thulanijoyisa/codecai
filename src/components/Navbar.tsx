51f4c1e0b6516e51bdfa00d19d3527e3fa9c5fa8
thulanijoyisa <thulanijoyisa@gmail.com>
2024-03-15 15:10:07 +0200

    commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,88 @@
+import Link from 'next/link'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import { buttonVariants } from './ui/button'
+import {
+  LoginLink,
+  RegisterLink,
+  getKindeServerSession,
+} from '@kinde-oss/kinde-auth-nextjs/server'
+import { ArrowRight } from 'lucide-react'
+
+
+const Navbar = () => {
+    const { getUser } = getKindeServerSession()
+    const user = getUser()
+  
+    return (
+      <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+        <MaxWidthWrapper>
+          <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
+            <Link
+              href='/'
+              className='flex z-40 font-semibold'>
+              <span>Codecia.</span>
+            </Link>
+  
+            <div className='hidden items-center space-x-4 sm:flex'>
+              {!user ? (
+                <>
+                  <Link
+                    href='/pricing'
+                    className={buttonVariants({
+                      variant: 'ghost',
+                      size: 'sm',
+                    })}>
+                    Pricing
+                  </Link>
+                  <LoginLink
+                    className={buttonVariants({
+                      variant: 'ghost',
+                      size: 'sm',
+                    })}>
+                    Sign in
+                  </LoginLink>
+                  <RegisterLink
+                    className={buttonVariants({
+                      size: 'sm',
+                    })}>
+                    Get started{' '}
+                    <ArrowRight className='ml-1.5 h-5 w-5' />
+                  </RegisterLink>
+                </>
+              ) : (
+                <>
+                  <Link
+                    href='/dashboard'
+                    className={buttonVariants({
+                      variant: 'ghost',
+                      size: 'sm',
+                    })}>
+                    PDF Chats Dashboard
+                  </Link>
+                  <Link
+                    href='/gallery'
+                    className={buttonVariants({
+                      variant: 'ghost',
+                      size: 'sm',
+                    })}>
+                    Courses Dashboard
+                  </Link>
+                  <Link
+                    href='/quiz'
+                    className={buttonVariants({
+                      variant: 'ghost',
+                      size: 'sm',
+                    })}>
+                    Quizes Dashboard
+                  </Link>
+
+                </>
+              )}
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </nav>
+    )
+  }
+  
+  export default Navbar
